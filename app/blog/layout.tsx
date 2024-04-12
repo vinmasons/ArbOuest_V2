7669d3297dee2210c49bd649cf3d9dbfd678a119
elevaseo <75243836+bastienallain@users.noreply.github.com>
2024-04-12 11:02:58 +0700

    Update

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -3,12 +3,8 @@ import { VisualEditing, toPlainText } from 'next-sanity';
 import { draftMode } from 'next/headers';
 import '../globals.css';
 
-import localFont from 'next/font/local';
-import Footer from '@components/Footer';
 import Navbar from '@components/Navbar';
-import clsx from 'clsx';
 
-import Script from 'next/script';
 import AlertBanner from './alert-banner';
 
 import * as demo from '@/sanity/lib/demo';
@@ -48,63 +44,18 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-const degular = localFont({
-  src: [
-    {
-      path: '../components/fonts/Degular-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../components/fonts/Degular-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../components/fonts/Degular-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../components/fonts/Degular-Black.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
-  display: 'swap',
-  variable: '--font-degular',
-});
-
 export default function BlogLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="fr"
-      className={clsx(
-        'font-sans scroll-smooth bg-soft-cream antialiased',
-        degular.variable
-      )}
-    >
-      <Script
-        strategy="worker"
-        type="text/javascript"
-        src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=RN4ngH"
-      ></Script>
-      <body>
-        <Suspense>
-          {' '}
-          <Navbar />
-        </Suspense>
+    <section>
+      {draftMode().isEnabled && <AlertBanner />}
 
-        {draftMode().isEnabled && <AlertBanner />}
+      {children}
 
-        {children}
-
-        {draftMode().isEnabled && <VisualEditing />}
-      </body>
-    </html>
+      {draftMode().isEnabled && <VisualEditing />}
+    </section>
   );
 }
